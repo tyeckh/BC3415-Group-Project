b45094f4d163d7d4298ba1faea809957d7ba51f5
Chiang Qin Zhi <109030951+chqizh@users.noreply.github.com>
2024-11-05 02:05:43 +0800

    update

--- static/easter-egg.js
+++ static/easter-egg.js
@@ -1,17 +1,31 @@
 // easter-egg.js
 let clickCount = 0;
+let isFlipped = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const flipContainer = document.querySelector(".flip-card");
+  try {
+    const flipContainer = document.querySelector(".flip-card");
 
-  flipContainer.addEventListener("click", () => {
-    clickCount++;
-    if (clickCount === 3) {
-      // Add the flipped class to trigger the animation
-      document.querySelector('.flip-card').classList.toggle('flipped');
+    flipContainer.addEventListener("click", () => {
+      clickCount++;
+      if (isFlipped == true) {
+        // Add the flipped class to trigger the animation
+        document.querySelector('.flip-card').classList.toggle('flipped');
+        // Reset click count after flipping
+        clickCount = 0;
+        isFlipped = false;
+      }
+      if (clickCount === 5) {
+        // Add the flipped class to trigger the animation
+        document.querySelector('.flip-card').classList.toggle('flipped');
 
-      // Reset click count after flipping
-      clickCount = 0;
-    }
-  });
+        // Reset click count after flipping
+        clickCount = 0;
+        isFlipped = true;
+      }
+    });
+  }
+  catch (error) {
+    console.error("Error:", error);
+  }
 });
--- static/script.js
+++ static/script.js
@@ -274,25 +274,27 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 //Email Function
-emailjs.init({publicKey: 'TNxXByXHi_TSiUtj_'});
-document.getElementById("contact-form").addEventListener("submit", () =>{
+emailjs.init({ publicKey: "TNxXByXHi_TSiUtj_" });
+document.getElementById("contact-form").addEventListener("submit", () => {
   // Prevent the default form submission
   event.preventDefault();
 
   let parms = {
-      contact_name: document.getElementById("contact_name").value,
-      email: document.getElementById("email").value,
-      subject: document.getElementById("subject").value,
-      message: document.getElementById("message").value,
+    contact_name: document.getElementById("contact_name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
   };
-  
-  emailjs.send('service_0uo416r', 'template_rqk459a', parms)
-  .then(() => {
+
+  emailjs.send("service_0uo416r", "template_rqk459a", parms).then(
+    () => {
       alert("Email Sent!"); // Success alert
-  }, function(error) {
+    },
+    function (error) {
       alert("Failed to send email. Please try again later."); // Error alert
-      console.error('EmailJS Error:', error); // Log the error for debugging
-  });
+      console.error("EmailJS Error:", error); // Log the error for debugging
+    }
+  );
 });
 
 console.log("script.js loaded successfully");
